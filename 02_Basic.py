4d69fd6088f45510b30233e4db104c696b8a7631
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2025-04-20 11:13:38 +0530

    Read Me

---
+++ 02_Basic.py
@@ -0,0 +1,41 @@
+# 👉Addition of Two number
+print(20 + 3)
+
+# 👉Subtraction of Two number
+print(20 - 3)
+
+# 👉Multiplication of Two number
+print(20 * 3)
+
+# 👉Division of Two number
+print(20 // 3)  #here // is also called the Integer division and floor division
+
+# 👉All combined
+print(8 + 2 - 3 )
+
+print(8 * 2 - 3)
+
+print((8 + 2) * 3)
+
+# 👉Power of 2 power 3
+print(2 ** 3)
+
+# 👉Modular of Two number
+print(10 % 3)
+
+# 👉Print the string
+# print() is the in-built function in python
+print('Twinkle')
+
+print("Twinkle's Laptop")
+
+print('Twinkle "Laptop"')
+
+# 👉Concatenate string
+print('Twinkle' + 'Twinkle')
+
+# 👉Print 10 time Twinkle
+print(10 * 'Twinkle')
+
+# 👉r means raw string
+print(r'd:\docs\Twinkle')
---
+++ 03_Variable.py
@@ -0,0 +1,48 @@
+# 👉Variables
+
+x = 2
+print(x + 3)
+
+y = 8
+print(x + 2)
+
+x = 9
+print(x + y)
+
+print(y)
+
+print(x + 10)
+
+_ = 8
+print(_ + y)
+
+# _ means represent output for the previous operation
+
+name = 'You tube'
+print(name)
+
+print(name + ' rockes')
+
+# 👉Fetch one character of the string
+# 👉Here  -number means -1 , -2 is start the right to left and not
+# include 0 but left to right include 0 ,1 ,2 etc...
+name = 'Twinkle'
+print(name[2])
+
+print(name[0:2])
+
+print(name[1:4])
+
+print(name[1:])
+
+print(name[:4])
+
+# 👉Sting is immutable means it can not change the value.
+
+print('my ' + name[3:])
+
+# 👉len() is the inbuilt function in python means is to find the sting
+# character of length.
+
+myname = 'Twinkle Pansuriya'
+print(len(myname))
